c9d7d55efda4c362144e1d622a458e6077e38f38
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-21 14:29:06 -0700

    added first and last name fields to new user registration form and add functionality to create new boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,25 @@
 class BoardsController < ApplicationController
+
   def index
+    @boards = current_user.boards
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.create(params.require(:board).permit(:title))
+
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+
   end
 
   def edit
