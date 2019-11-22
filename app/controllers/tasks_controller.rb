67de0fc1fd1deeb4c8a0158bff07488e2717cbfe
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-22 15:18:35 -0700

    add functionality to edit and delete tasks as well as order lists and tasks by priority

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_list
   before_action :set_board
+  before_action :set_task, only: [:edit, :update, :destroy]
 
   def new
     @task = @list.tasks.new
   end
 
   def create
-    @task = @list.tasks.new(params.require(:task).permit(:title, :priority))
+    @task = @list.tasks.new(task_params)
 
     if @task.save
       redirect_to board_path(@board)
@@ -20,6 +21,21 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def update
+
+    if @task.update(task_params)
+      redirect_to board_path(@board)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to board_path(@board)
+  end
+
   private
 
     def set_board
@@ -28,6 +44,14 @@ class TasksController < ApplicationController
 
     def set_list
       @list = List.find(params[:list_id])
+    end
+
+    def set_task
+      @task = @list.tasks.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :priority)
     end
 
 end
