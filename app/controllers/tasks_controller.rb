2567b782d0e5e41f57f189fad3f2faf067bfe355
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-22 14:24:36 -0700

    added functionality to create new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,33 @@
 class TasksController < ApplicationController
+  before_action :set_list
+  before_action :set_board
+
   def new
+    @task = @list.tasks.new
+  end
+
+  def create
+    @task = @list.tasks.new(params.require(:task).permit(:title, :priority))
+
+    if @task.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+
   end
 
   def edit
   end
+
+  private
+
+    def set_board
+      @board = Board.find(@list.board_id)
+    end
+
+    def set_list
+      @list = List.find(params[:list_id])
+    end
+
 end
